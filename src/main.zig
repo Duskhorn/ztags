bf746474db107298c7e9a00d41c65f44224ea0b0
Duskhorn <uwueclipsedhorn@gmail.com>
2022-05-09 14:11:01 +0200

    it loads files now

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,30 @@
 const std = @import("std");
 
 pub fn main() anyerror!void {
-    const args = std.process.args();
-    for (args.next()) |arg| {
-        std.log.debug("{s}", .{arg});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    var alloc = gpa.allocator();
+    var args = std.process.args();
+    defer args.deinit();
+
+    //ignore executable name
+    const executable = try args.next(alloc).?;
+    defer alloc.free(executable);
+
+    while (args.next(alloc)) |arg_err| {
+        const path = try arg_err;
+        std.log.debug("{s}", .{path});
+        defer alloc.free(path);
+
+        const source = std.fs.cwd().readFileAllocOptions(alloc, path, std.math.maxInt(usize), null, @alignOf(u8), 0) catch |err| {
+            std.log.warn("Error in opening file '{s}' : {}", .{ path, err });
+            continue;
+        };
+        defer alloc.free(source);
+
+        var tree = try std.zig.parse(alloc, source);
+        defer tree.deinit(alloc);
     }
 }
 
